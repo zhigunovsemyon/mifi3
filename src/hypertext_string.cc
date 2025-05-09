9edb30cd337b29d9de3a2a3dd778a0236af3b13c
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-05-09 12:21:38 +0300

    Добавил прототипы внутренних функций, работающих со string_view

--- src/hypertext_string.cc
+++ src/hypertext_string.cc
@@ -1,32 +1,43 @@
 #include "hypertext_string.h"
+#include <string>
 #include <string_view>
+
+static std::string _to_hypertext(std::string_view src)
+{
+	return std::string{src};
+}
+
+static std::string _from_hypertext(std::string_view src)
+{
+	return std::string{src};
+}
 
 std::string to_hypertext(std::string const & src)
 {
-	return src;
+	return _to_hypertext(src);
 }
 
 std::string to_hypertext(char const * src)
 {
-	return src;
+	return _to_hypertext(src);
 }
 
 std::string to_hypertext(char const * src, size_t length)
 {
-	return {src, length};
+	return _to_hypertext({src,length});
 }
 
 std::string from_hypertext(std::string const & src)
 {
-	return src;
+	return _from_hypertext(src);
 }
 
 std::string from_hypertext(char const * src)
 {
-	return src;
+	return _from_hypertext(src);
 }
 
 std::string from_hypertext(char const * src, size_t length)
 {
-	return {src, length};
+	return _from_hypertext({src,length});
 }
